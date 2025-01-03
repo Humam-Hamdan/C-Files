545b02e3b70e4b1c26110c3486e59b1524a78f63
Manga_Killer <58117805+Humam-Hamdan@users.noreply.github.com>
2025-01-03 23:35:18 +0100

    Aufgabe 19 Blatt 10 Init

---
+++ 10/INF1 - Loesung Aufgabe 19.c
@@ -0,0 +1,15 @@
+/**************************************/
+/* Humam Hamdan                       */
+/* Gruppe: Do.12                      */
+/* Leiter: Florian Möschler           */
+/* Uebungsblatt:  10                  */
+/* Aufgabe:      19                   */
+/* Datum:       3 jan                 */
+/**************************************/
+
+/*
+So, we should read two strings of length 20 and then search for them in a string of length 20*10 
+
+Have no idea really, work in progress.
+
+*/
