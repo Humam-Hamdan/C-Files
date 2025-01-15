1a91bb70ca1763b1d5653123c7a051aee24d3c50
Manga_Killer <58117805+Humam-Hamdan@users.noreply.github.com>
2025-01-15 01:34:00 +0100

    Blatt11

--- 11/INF1 - Loesung Aufgabe 21.c
+++ 11/INF1 - Loesung Aufgabe 21.c
@@ -2,7 +2,39 @@
 /* Humam Hamdan                       */
 /* Gruppe: Do.12                      */
 /* Leiter: Florian Möschler           */
-/* Uebungsblatt:                      */
-/* Aufgabe:                           */
-/* Datum:                             */
+/* Uebungsblatt:11                      */
+/* Aufgabe:   21                        */
+/* Datum:     15Jan                        */
 /**************************************/
+
+/*
+ * STRUCT with 1 int 2 floats.
+ * Function to read & write
+ * funktion to get the endresult
+*/
+
+#include <stdio.h>
+
+typedef struct Werbung{
+    int Anzahl;
+    float P; // P für probability
+    float Eur; // Euro
+} Werbung;
+
+
+
+void ReadWrite(Werbung *wer){
+    printf("Zahl der Werbung? \nClick Wahrscheinlichkeit? \nwie viel Euro pro Click?\n");
+    scanf("%d %f %f", &(wer->Anzahl), &(wer->P), &(wer->Eur));
+}
+
+float MulWerb(Werbung wer){
+    return wer.Anzahl * wer.P * wer.Eur;
+}
+
+int main(){
+    Werbung wer;
+    ReadWrite(&wer);
+    printf("Gewinn ist %.2f €\n" ,MulWerb(wer));
+    return 0;
+}
--- 11/INF1 - Loesung Aufgabe 22.c
+++ 11/INF1 - Loesung Aufgabe 22.c
@@ -2,7 +2,29 @@
 /* Humam Hamdan                       */
 /* Gruppe: Do.12                      */
 /* Leiter: Florian Möschler           */
-/* Uebungsblatt:                      */
-/* Aufgabe:                           */
-/* Datum:                             */
+/* Uebungsblatt:11                      */
+/* Aufgabe:    22                       */
+/* Datum:     15jan                        */
 /**************************************/
+
+/*
+ * Determine Number of Participants
+ * Print a Line and expect the length of the number as input
+ * save input
+ * set passing grade
+ * function which gives the name & number & grade of those who passed
+*/
+
+#include <stdio.h>
+
+typedef struct Note{
+    char Nachname[30];
+    int MatrikelNum;
+    int Note;
+}Note;
+
+void RedaWrite(Note *n){
+
+}
+
+// do not really know how to make it interactive, to be determined.
